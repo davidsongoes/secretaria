8e63d6771a479dd68bd838ec37f832405edd8e67
3S SIN Góes <davidsongoes@gmail.com>
2018-03-06 15:43:55 -0300

    Desativando droppable calendario aulas

--- js/demo/fullcalendar-aulas.js
+++ js/demo/fullcalendar-aulas.js
@@ -38,7 +38,7 @@ $(document).ready(function () {
         timeFormat: 'H(:mm)',
         displayEventTime: true,
         editable: true,
-        droppable: true, // this allows things to be dropped onto the calendar
+        // droppable: true, // this allows things to be dropped onto the calendar
         drop: function () {
             // is the "remove after drop" checkbox checked?
             if ($('#drop-remove').is(':checked')) {
@@ -50,32 +50,6 @@ $(document).ready(function () {
             alert('Evento: '+event.title);
         },
         eventRender: function (event, element) {
-            if (event.title == 'Café da Manhã'){
-                element.css({
-                    'background-color':'#ed5565',
-                    'color':'#ffffff',
-                    'border-color': '#ed5565'
-
-                });
-            }if (event.title == 'Almoço'){
-                element.css({
-                    'background-color':'#1ab394',
-                    'color':'#ffffff',
-                    'border-color': '#1ab394'
-                });
-            }if (event.title == 'Jantar'){
-                element.css({
-                    'background-color':'#f8ac59',
-                    'color':'#ffffff',
-                    'border-color': '#f8ac59'
-                });
-            }if (event.title == 'Ceia'){
-                element.css({
-                    'background-color':'#1c84c6',
-                    'color':'#ffffff',
-                    'border-color': '#1c84c6'
-                });
-            }
             element.append("<span class='closeon'>X</span>");
             element.find(".closeon").click(function () {
                 $('#calendarAulas').fullCalendar('removeEvents', event._id);
