cf9800615d2123ba09f9ea591b58591cd27f92a0
Davidson Góes <davidsongoes@gmail.com>
2018-03-07 21:16:26 -0300

    Comentando linhas

--- js/demo/chartjs-demo-enturmacao-controle-cfoav.js
+++ js/demo/chartjs-demo-enturmacao-controle-cfoav.js
@@ -75,144 +75,144 @@ $(function () {
     new Chart(ctx2018, {type: 'bar', data: barData2018, options: barOptions2018});
 
 
-    // Gráfico Barra CFOAV 2017
-    var barData2017 = {
-        labels: ["2014", "2015", "2016", "2017"],
-        datasets: [
-            {
-                label: "Vagas Edital",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [160, 160, 160, 160]
-            },
-            {
-                label: "Matriculados",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [145, 145, 145, 145]
-            },
-            {
-                label: "Liminaristas",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [6, 6, 5, 4]
-            },
-            {
-                label: "Desligados",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [10, 10, 5, 2]
-            },
-            {
-                label: "ONA",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [5, 5, 4, 3]
-            },
-            {
-                label: "Total",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [130, 120, 113, 109]
-            }
-        ]
-    };
-
-    var barOptions2017 = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        },
-        responsive: true
-    };
-
-
-    var ctx2017 = document.getElementById("barChartCfoav2017EmAndamento").getContext("2d");
-    new Chart(ctx2017, {type: 'bar', data: barData2017, options: barOptions2017});
-
-
-// Gráfico Barra CFOAV 2017
-    var barData2016 = {
-        labels: ["2013", "2014", "2015", "2016"],
-        datasets: [
-            {
-                label: "Vagas Edital",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [160, 160, 160, 160]
-            },
-            {
-                label: "Matriculados",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [145, 145, 145, 145]
-            },
-            {
-                label: "Liminaristas",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [6, 6, 5, 4]
-            },
-            {
-                label: "Desligados",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [10, 10, 5, 2]
-            },
-            {
-                label: "ONA",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [5, 5, 4, 3]
-            },
-            {
-                label: "Total",
-                backgroundColor: randomColorGenerator(),
-                borderColor: "#000",
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [130, 120, 113, 109]
-            }
-        ]
-    };
-
-    var barOptions2016 = {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
-        },
-        responsive: true
-    };
-
-
-    var ctx2016 = document.getElementById("barChartCfoav2016EmAndamento").getContext("2d");
-    new Chart(ctx2016, {type: 'bar', data: barData2016, options: barOptions2016});
+//     // Gráfico Barra CFOAV 2017
+//     var barData2017 = {
+//         labels: ["2014", "2015", "2016", "2017"],
+//         datasets: [
+//             {
+//                 label: "Vagas Edital",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [160, 160, 160, 160]
+//             },
+//             {
+//                 label: "Matriculados",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [145, 145, 145, 145]
+//             },
+//             {
+//                 label: "Liminaristas",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [6, 6, 5, 4]
+//             },
+//             {
+//                 label: "Desligados",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [10, 10, 5, 2]
+//             },
+//             {
+//                 label: "ONA",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [5, 5, 4, 3]
+//             },
+//             {
+//                 label: "Total",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [130, 120, 113, 109]
+//             }
+//         ]
+//     };
+//
+//     var barOptions2017 = {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         },
+//         responsive: true
+//     };
+//
+//
+//     var ctx2017 = document.getElementById("barChartCfoav2017EmAndamento").getContext("2d");
+//     new Chart(ctx2017, {type: 'bar', data: barData2017, options: barOptions2017});
+//
+//
+// // Gráfico Barra CFOAV 2017
+//     var barData2016 = {
+//         labels: ["2013", "2014", "2015", "2016"],
+//         datasets: [
+//             {
+//                 label: "Vagas Edital",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [160, 160, 160, 160]
+//             },
+//             {
+//                 label: "Matriculados",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [145, 145, 145, 145]
+//             },
+//             {
+//                 label: "Liminaristas",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [6, 6, 5, 4]
+//             },
+//             {
+//                 label: "Desligados",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [10, 10, 5, 2]
+//             },
+//             {
+//                 label: "ONA",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [5, 5, 4, 3]
+//             },
+//             {
+//                 label: "Total",
+//                 backgroundColor: randomColorGenerator(),
+//                 borderColor: "#000",
+//                 pointBackgroundColor: "rgba(26,179,148,1)",
+//                 pointBorderColor: "#fff",
+//                 data: [130, 120, 113, 109]
+//             }
+//         ]
+//     };
+//
+//     var barOptions2016 = {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         },
+//         responsive: true
+//     };
+//
+//
+//     var ctx2016 = document.getElementById("barChartCfoav2016EmAndamento").getContext("2d");
+//     new Chart(ctx2016, {type: 'bar', data: barData2016, options: barOptions2016});
 });
