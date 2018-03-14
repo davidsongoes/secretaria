0b04c2a0ad67b4e4c44878edeeb1260af3bb7c5a
3S SIN Góes <davidsongoes@gmail.com>
2018-03-14 10:18:52 -0300

    Refatorando gráfico - Discente

--- js/demo/chartjs-demo-dimensao-discente.js
+++ js/demo/chartjs-demo-dimensao-discente.js
@@ -1,16 +1,32 @@
 $(function () {
-    //Cor do background aleatória RGB
-    var randomColorGeneratorRgb = function () {
-        var graphColors1 = [];
+    // Cor do background aleatória RGB
+    var randomColorGeneratorRgb = function (flag) {
         var randomR = Math.floor((Math.random() * 130) + 100);
         var randomG = Math.floor((Math.random() * 130) + 100);
         var randomB = Math.floor((Math.random() * 130) + 100);
 
-        var graphBackground1 = "rgb("
-            + randomR + ", "
-            + randomG + ", "
-            + randomB + ")";
-        return graphBackground1;
+        if (flag == 'back') {
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 0.4)";
+        }if( flag == 'borda'){
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 1)";
+        }if(flag == 'ponto'){
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 1)";
+        }if(flag == 'back-forte'){
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 0.8)";
+        }
+        return graphBackground4;
     };
 
 
@@ -28,26 +44,26 @@ $(function () {
         var randomG = Math.floor((Math.random() * 130) + 100);
         var randomB = Math.floor((Math.random() * 130) + 100);
 
-        var graphBackground1 = "rgb("
+        var graphBackground1 = "rgba("
             + randomR + ", "
             + randomG + ", "
-            + randomB + ")";
+            + randomB + ", 0.7)";
         graphColors1.push(graphBackground1);
 
-        var graphOutline1 = "rgb("
+        var graphOutline1 = "rgba("
             + (randomR - 80) + ", "
             + (randomG - 80) + ", "
-            + (randomB - 80) + ")";
+            + (randomB - 80) + ", 0.8)";
         graphOutlines1.push(graphOutline1);
 
-        var hoverColors1 = "rgb("
+        var hoverColors1 = "rgba("
             + (randomR + 25) + ", "
             + (randomG + 25) + ", "
-            + (randomB + 25) + ")";
+            + (randomB + 25) + ", 1)";
         hoverColor1.push(hoverColors1);
 
         i++;
-    };
+    }
 
     //Cor do background aleatória RGB por linha 2
     var internalData2 = [65, 59, 80, 81, 56];
@@ -59,35 +75,35 @@ $(function () {
     var internalDataLength2 = internalData2.length;
     i = 0;
     while (i <= internalDataLength2) {
-        var randomR = Math.floor((Math.random() * 130) + 100);
-        var randomG = Math.floor((Math.random() * 130) + 100);
-        var randomB = Math.floor((Math.random() * 130) + 100);
+        var randomR1 = Math.floor((Math.random() * 130) + 100);
+        var randomG1 = Math.floor((Math.random() * 130) + 100);
+        var randomB1 = Math.floor((Math.random() * 130) + 100);
 
-        var graphBackground2 = "rgb("
-            + randomR + ", "
-            + randomG + ", "
-            + randomB + ")";
+        var graphBackground2 = "rgba("
+            + randomR1 + ", "
+            + randomG1 + ", "
+            + randomB1 + ", 0.4)";
         graphColors2.push(graphBackground2);
 
-        var graphOutline2 = "rgb("
-            + (randomR - 80) + ", "
-            + (randomG - 80) + ", "
-            + (randomB - 80) + ")";
+        var graphOutline2 = "rgba("
+            + (randomR1 - 80) + ", "
+            + (randomG1 - 80) + ", "
+            + (randomB1 - 80) + ", 0.7)";
         graphOutlines2.push(graphOutline2);
 
-        var hoverColors2 = "rgb("
-            + (randomR + 25) + ", "
-            + (randomG + 25) + ", "
-            + (randomB + 25) + ")";
+        var hoverColors2 = "rgba("
+            + (randomR1 + 25) + ", "
+            + (randomG1 + 25) + ", "
+            + (randomB1 + 25) + ", 1)";
         hoverColor2.push(hoverColors2);
 
         i++;
-    };
+    }
 
 
     // Gráfico 1 - Barra
     var barData1 = {
-        labels: ["Excelente", "Ótima", "Boa", "Ruím", "Péssima"],
+        labels: ["Excelente", "Ótimo", "Bom", "Ruím", "Péssimo"],
         datasets: [
             {
                 backgroundColor: graphColors1,
@@ -126,31 +142,19 @@ $(function () {
 
     // Gráfico 2 - Barra
     var barData2 = {
-        labels: ["Bastante Adequados", "Adequados", "Parcialamente Adequados", "Pouco Adequados", "Inadequados"],
+        labels: ["Matemática", "Português", "Física", "Geometria"],
         datasets: [
             {
-                label: "CFOAV",
-                backgroundColor: randomColorGeneratorRgb(),
+                label: "Exame Final",
+                backgroundColor: randomColorGeneratorRgb('back-forte'),
                 borderColor: graphOutlines1,
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [60, 35, 3, 1, 1]
+                data: [15, 20, 10, 12]
             },
             {
-                label: "CFOINT",
-                backgroundColor: randomColorGeneratorRgb(),
+                label: "Segunda época",
+                backgroundColor: randomColorGeneratorRgb('back-forte'),
                 borderColor: graphOutlines1,
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [72, 25, 1, 1, 1]
-            },
-            {
-                label: "CFOINF",
-                backgroundColor: randomColorGeneratorRgb(),
-                borderColor: graphOutlines1,
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [80, 19, 1, 0, 0]
+                data: [10, 14, 7, 5]
             }
         ]
     };
@@ -161,8 +165,8 @@ $(function () {
                 ticks: {
                     beginAtZero: true,
                     steps: 10,
-                    stepValue: 5,
-                    max: 100
+                    stepValue: 5
+                    // max: 25
                 }
             }]
         },
@@ -173,23 +177,23 @@ $(function () {
     var ctx2 = document.getElementById("chartDiscente2").getContext("2d");
     new Chart(ctx2, {type: 'horizontalBar', data: barData2, options: barOptions2});
 
-//    Gráfico 3 - Doughnut
-    var doughnutData = {
-        labels: ["Excelente", "Ótima", "Boa", "Ruím", "Péssima"],
-        datasets: [{
-            data: [80,11,8,1,0],
-            backgroundColor: [randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(), randomColorGeneratorRgb()]
-        }]
-    } ;
-
-
-    var doughnutOptions = {
-        responsive: true
-    };
-
-
-    var ctx3 = document.getElementById("chartDiscente3").getContext("2d");
-    new Chart(ctx3, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
+// //    Gráfico 3 - Doughnut
+//     var doughnutData = {
+//         labels: ["Excelente", "Ótima", "Boa", "Ruím", "Péssima"],
+//         datasets: [{
+//             data: [80,11,8,1,0],
+//             backgroundColor: [randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(), randomColorGeneratorRgb()]
+//         }]
+//     } ;
+//
+//
+//     var doughnutOptions = {
+//         responsive: true
+//     };
+//
+//
+//     var ctx3 = document.getElementById("chartDiscente3").getContext("2d");
+//     new Chart(ctx3, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
 
 //    Gráfico 4 - Polar
 
@@ -198,7 +202,7 @@ $(function () {
             data: [
                 60,25,10,5
             ],
-            backgroundColor: [randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb()],
+            backgroundColor: [randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte')],
             label: [
                 "My Radar chart"
             ]
