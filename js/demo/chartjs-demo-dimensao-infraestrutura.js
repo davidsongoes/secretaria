d4c6a08f57505add6e36998bd38f67e1e329765b
3S SIN Góes <davidsongoes@gmail.com>
2018-03-14 12:48:03 -0300

    Refatorando gráfico - Infraestrutura

--- js/demo/chartjs-demo-dimensao-infraestrutura.js
+++ js/demo/chartjs-demo-dimensao-infraestrutura.js
@@ -1,16 +1,35 @@
 $(function () {
-    //Cor do background aleatória RGB
-    var randomColorGeneratorRgb = function () {
-        var graphColors1 = [];
-        var randomR = Math.floor((Math.random() * 130) + 100);
-        var randomG = Math.floor((Math.random() * 130) + 100);
+    // Cor do background aleatória RGB
+    var randomColorGeneratorRgb = function (flag) {
+        var randomR = Math.floor((Math.random() * 120) + 100);
+        var randomG = Math.floor((Math.random() * 125) + 100);
         var randomB = Math.floor((Math.random() * 130) + 100);
 
-        var graphBackground1 = "rgb("
-            + randomR + ", "
-            + randomG + ", "
-            + randomB + ")";
-        return graphBackground1;
+        if (flag == 'back') {
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 0.4)";
+        }
+        if (flag == 'borda') {
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 1)";
+        }
+        if (flag == 'ponto') {
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 1)";
+        }
+        if (flag == 'back-forte') {
+            var graphBackground4 = "rgba("
+                + randomR + ", "
+                + randomG + ", "
+                + randomB + ", 0.8)";
+        }
+        return graphBackground4;
     };
 
 
@@ -28,26 +47,26 @@ $(function () {
         var randomG = Math.floor((Math.random() * 130) + 100);
         var randomB = Math.floor((Math.random() * 130) + 100);
 
-        var graphBackground1 = "rgb("
+        var graphBackground1 = "rgba("
             + randomR + ", "
             + randomG + ", "
-            + randomB + ")";"rgba(26,179,148,0.2)"
+            + randomB + ", 0.7)";
         graphColors1.push(graphBackground1);
 
-        var graphOutline1 = "rgb("
+        var graphOutline1 = "rgba("
             + (randomR - 80) + ", "
             + (randomG - 80) + ", "
-            + (randomB - 80) + ")";
+            + (randomB - 80) + ", 0.8)";
         graphOutlines1.push(graphOutline1);
 
-        var hoverColors1 = "rgb("
+        var hoverColors1 = "rgba("
             + (randomR + 25) + ", "
             + (randomG + 25) + ", "
-            + (randomB + 25) + ")";
+            + (randomB + 25) + ", 1)";
         hoverColor1.push(hoverColors1);
 
         i++;
-    };
+    }
 
     //Cor do background aleatória RGB por linha 2
     var internalData2 = [65, 59, 80, 81, 56];
@@ -59,30 +78,30 @@ $(function () {
     var internalDataLength2 = internalData2.length;
     i = 0;
     while (i <= internalDataLength2) {
-        var randomR = Math.floor((Math.random() * 130) + 100);
-        var randomG = Math.floor((Math.random() * 130) + 100);
-        var randomB = Math.floor((Math.random() * 130) + 100);
+        var randomR1 = Math.floor((Math.random() * 130) + 100);
+        var randomG1 = Math.floor((Math.random() * 130) + 100);
+        var randomB1 = Math.floor((Math.random() * 130) + 100);
 
-        var graphBackground2 = "rgb("
-            + randomR + ", "
-            + randomG + ", "
-            + randomB + ")";
+        var graphBackground2 = "rgba("
+            + randomR1 + ", "
+            + randomG1 + ", "
+            + randomB1 + ", 0.6)";
         graphColors2.push(graphBackground2);
 
-        var graphOutline2 = "rgb("
-            + (randomR - 80) + ", "
-            + (randomG - 80) + ", "
-            + (randomB - 80) + ")";
+        var graphOutline2 = "rgba("
+            + (randomR1 - 80) + ", "
+            + (randomG1 - 80) + ", "
+            + (randomB1 - 80) + ", 0.7)";
         graphOutlines2.push(graphOutline2);
 
-        var hoverColors2 = "rgb("
-            + (randomR + 25) + ", "
-            + (randomG + 25) + ", "
-            + (randomB + 25) + ")";
+        var hoverColors2 = "rgba("
+            + (randomR1 + 25) + ", "
+            + (randomG1 + 25) + ", "
+            + (randomB1 + 25) + ", 1)";
         hoverColor2.push(hoverColors2);
 
         i++;
-    };
+    }
 
 
     // Gráfico 1 - Barra
@@ -129,28 +148,11 @@ $(function () {
         labels: ["Bastante Adequados", "Adequados", "Parcialamente Adequados", "Pouco Adequados", "Inadequados"],
         datasets: [
             {
-                label: "CFOAV",
-                backgroundColor: randomColorGeneratorRgb(),
-                borderColor: graphOutlines1,
+                backgroundColor: [randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte')],
+                // borderColor: graphOutlines1,
                 pointBackgroundColor: "rgba(26,179,148,1)",
                 pointBorderColor: "#fff",
-                data: [60, 35, 3, 1, 1]
-            },
-            {
-                label: "CFOINT",
-                backgroundColor: randomColorGeneratorRgb(),
-                borderColor: graphOutlines1,
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [72, 25, 1, 1, 1]
-            },
-            {
-                label: "CFOINF",
-                backgroundColor: randomColorGeneratorRgb(),
-                borderColor: graphOutlines1,
-                pointBackgroundColor: "rgba(26,179,148,1)",
-                pointBorderColor: "#fff",
-                data: [80, 19, 1, 0, 0]
+                data: [85, 10, 3, 1, 1]
             }
         ]
     };
@@ -165,6 +167,9 @@ $(function () {
                     max: 100
                 }
             }]
+        },
+        legend: {
+            display: false
         },
         responsive: true
     };
