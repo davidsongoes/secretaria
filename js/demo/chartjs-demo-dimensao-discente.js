333322223cd06e1de5d2f6dd76a82af303c07ae4
3S SIN Góes <davidsongoes@gmail.com>
2018-03-15 13:13:54 -0300

    refatorando cores dos gráficos - Discente

--- js/demo/chartjs-demo-dimensao-discente.js
+++ js/demo/chartjs-demo-dimensao-discente.js
@@ -1,4 +1,41 @@
 $(function () {
+    //Cor básica
+    var colorFunction = function (flag) {
+        var color = '';
+        if (flag == 'navy-high') {
+            color = 'rgba(26,179,148,0.8)';
+        }
+        if (flag == 'navy-mid') {
+            color = 'rgba(26,179,148,0.6)';
+        }if (flag == 'navy-low') {
+            color = 'rgba(26,179,148,0.4)';
+        }if (flag == 'yellow-high') {
+            color = 'rgba(248,172,89,0.8)';
+        }if (flag == 'yellow-mid') {
+            color = 'rgba(248,172,89,0.6)';
+        }if (flag == 'yellow-low') {
+            color = 'rgba(248,172,89,0.4)';
+        }if (flag == 'lazur-high') {
+            color = 'rgba(35,198,200,0.8)';
+        }if (flag == 'lazur-mid') {
+            color = 'rgba(35,198,200,0.6)';
+        }if (flag == 'lazur-low') {
+            color = 'rgba(35,198,200,0.4)';
+        }if (flag == 'red-high') {
+            color = 'rgba(237,85,101,0.8)';
+        }if (flag == 'red-mid') {
+            color = 'rgba(237,85,101,0.6)';
+        }if (flag == 'red-low') {
+            color = 'rgba(237,85,101,0.4)';
+        }if (flag == 'gray-high') {
+            color = 'rgba(103, 106, 108,0.8)';
+        }if (flag == 'gray-mid') {
+            color = 'rgba(103, 106, 108,0.6)';
+        }if (flag == 'gray-low') {
+            color = 'rgba(103, 106, 108,0.4)';
+        }
+        return color;
+    };
     // Cor do background aleatória RGB
     var randomColorGeneratorRgb = function (flag) {
         var randomR = Math.floor((Math.random() * 130) + 100);
@@ -106,10 +143,10 @@ $(function () {
         labels: ["Excelente", "Ótimo", "Bom", "Ruím", "Péssimo"],
         datasets: [
             {
-                backgroundColor: graphColors1,
+                backgroundColor: [colorFunction('navy-mid'),colorFunction('yellow-mid'), colorFunction('red-mid'), colorFunction('lazur-mid'), colorFunction('gray-mid')],
                 borderColor: graphOutlines1,
                 pointBackgroundColor: "rgba(26,179,148,1)",
-                hoverBackgroundColor: hoverColor1,
+                hoverBackgroundColor: 'rgba(103, 106, 108, 0.7)',
                 pointBorderColor: "#fff",
                 data: [60, 20, 10, 5, 5]
             }
@@ -146,20 +183,20 @@ $(function () {
         datasets: [
             {
                 label: "Exame Final",
-                backgroundColor: randomColorGeneratorRgb('back-forte'),
-                // borderColor: graphOutlines1,
+                backgroundColor: colorFunction('lazur-mid'),
+                borderColor: colorFunction('lazur-mid'),
                 data: [15, 20, 10, 12]
             },
             {
                 label: "Segunda época",
-                backgroundColor: randomColorGeneratorRgb('back-forte'),
-                // borderColor: graphOutlines1,
+                backgroundColor:colorFunction('yellow-mid'),
+                borderColor: colorFunction('yellow-mid'),
                 data: [10, 14, 7, 5]
             },
             {
                 label: "Reprovados",
-                backgroundColor: randomColorGeneratorRgb('back-forte'),
-                // borderColor: graphOutlines1,
+                backgroundColor: colorFunction('red-mid'),
+                borderColor: colorFunction('red-mid'),
                 data: [2, 3, 2, 4]
             }
         ]
@@ -182,52 +219,4 @@ $(function () {
 
     var ctx2 = document.getElementById("chartDiscente2").getContext("2d");
     new Chart(ctx2, {type: 'horizontalBar', data: barData2, options: barOptions2});
-
-// //    Gráfico 3 - Doughnut
-//     var doughnutData = {
-//         labels: ["Excelente", "Ótima", "Boa", "Ruím", "Péssima"],
-//         datasets: [{
-//             data: [80,11,8,1,0],
-//             backgroundColor: [randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(),randomColorGeneratorRgb(), randomColorGeneratorRgb()]
-//         }]
-//     } ;
-//
-//
-//     var doughnutOptions = {
-//         responsive: true
-//     };
-//
-//
-//     var ctx3 = document.getElementById("chartDiscente3").getContext("2d");
-//     new Chart(ctx3, {type: 'doughnut', data: doughnutData, options:doughnutOptions});
-
-//    Gráfico 4 - Polar
-
-    // var polarData = {
-    //     datasets: [{
-    //         data: [
-    //             60,25,10,5
-    //         ],
-    //         backgroundColor: [randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte'),randomColorGeneratorRgb('back-forte')],
-    //         label: [
-    //             "Gráfico"
-    //         ]
-    //     }],
-    //     labels: [
-    //         "Muito satisfatório","Satisfatório","Pouco satisfatório","Não satisfatório"
-    //     ]
-    // };
-    //
-    // var polarOptions = {
-    //     segmentStrokeWidth: 2,
-    //     beginAtZero: true,
-    //     steps: 10,
-    //     stepValue: 5,
-    //     max: 100
-    //
-    // };
-    //
-    // var ctx4 = document.getElementById("chartDiscente4").getContext("2d");
-    // new Chart(ctx4, {type: 'polarArea', data: polarData, options:polarOptions});
-
 });
